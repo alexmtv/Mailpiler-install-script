00db7a4e1bf923a6afa8fd9401d64b1b9cc2d15e
Alex M <a.matveev@lan-project.ru>
2017-07-05 19:18:48 +0300

    Draft install.sh
    Draft install.sh

---
+++ centos/7/install.sh
@@ -0,0 +1,21 @@
+yum -y update && yum -y install epel-release
+yum remove php-*
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum install php56w php56w-opcache php56w-common
+yum install php56w-mcrypt php56w-gd php56w-pdo php56w-pear php56w-gettext php56w-xml php56w-mysql php56w-intl php56w-mbstring php56w-bcmath php56w-ldap  php56w-pecl-memcached 
+yum -y install openssl mysql tre libzip sysstat mysql-connector-python && \
+yum -y install tcp_wrappers memcached catdoc libpst   poppler-utils unrtf tnef gcc make which cronie && \
+mkdir -p /usr/src/xlhtml-sj-mod && \
+curl -L https://bitbucket.org/jsuto/piler/downloads/xlhtml-0.5.1-sj-mod.tar.gz  | tar --strip-components=1 -xzC /usr/src/xlhtml-sj-mod && \
+cd /usr/src/xlhtml-sj-mod && ./configure && make && make install && \
+yum -y install http://sphinxsearch.com/files/sphinx-2.2.11-1.rhel7.x86_64.rpm  && \
+groupadd -g 1001 piler && useradd -u 1001 -g piler -m -s /bin/sh -d /var/piler piler && usermod -L piler && \
+mkdir -p /usr/src/piler && \
+curl -L https://bitbucket.org/jsuto/piler/downloads/piler-1.3.0.tar.gz  | tar --strip-components=1 -xzC /usr/src/piler && \
+cd /usr/src/piler && ./configure --localstatedir=/var --with-database=mysql --enable-tcpwrappers --enable-memcached && make && make install && \
+echo /usr/local/lib > /etc/ld.so.conf.d/local.conf && ldconfig && \
+cp /usr/local/etc/piler/sphinx.conf.dist /usr/local/etc/piler/sphinx.conf && \
+ln -sf /usr/local/etc/piler/sphinx.conf /etc/sphinx/sphinx.conf && \
+cp ./init.d/rc.searchd /etc/init.d/rc.searchd && chmod +x /etc/init.d/rc.searchd && chkconfig --add rc.searchd && \
+sed -e'/load_default_values$/q' ./util/postinstall.sh > /tmp/postinstall.sh && \
+cd /tmp && echo $'make_cron_entries\ncrontab -u $PILERUSER $CRON_TMP\nclean_up_temp_stuff' >> postinstall.sh && sh postinstall.sh && rm postinstall.sh && \
